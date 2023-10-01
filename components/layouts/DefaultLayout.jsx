b00bddbf8928f9bcebb66f298f5e7fdc544dad7a
Robin Rz <robinkurian1997@gmail.com>
2023-10-01 17:34:28 +0530

    addded jsconfig.json and fix relative imports to "@/"

--- components/layouts/DefaultLayout.jsx
+++ components/layouts/DefaultLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import DefaultFooter from './DefaultFooter'
-import DefaultHeader from './DefaultHeader'
+import DefaultFooter from '@/components/layouts/DefaultFooter'
+import DefaultHeader from '@/components/layouts/DefaultHeader'
 
 const DefaultLayout = ({ children, className }) => {
     return (
         <>
             <DefaultHeader />
-            <div className={`h-full ${className}`}>
+            <div className={`min-h-[calc(100vh-130px)] ${className}`}>
                 {children}
             </div>
             <DefaultFooter />
